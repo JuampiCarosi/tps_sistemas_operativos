27481b70fe892a31e0d0d8b36871cb04eec371bb
juampicarosi <juampicarosi@gmail.com>
2024-05-18 12:03:16 -0300

    init.c: run custom script

--- sched/kern/init.c
+++ sched/kern/init.c
@@ -68,7 +68,7 @@ i386_init(void)
 		ENV_CREATE(TEST, ENV_TYPE_USER);
 #else
 	// Touch all you want.
-	ENV_CREATE(user_hello, ENV_TYPE_USER);
+	ENV_CREATE(user_getpriority, ENV_TYPE_USER);
 	ENV_CREATE(user_hello, ENV_TYPE_USER);
 	ENV_CREATE(user_hello, ENV_TYPE_USER);
 #endif  // TEST*
