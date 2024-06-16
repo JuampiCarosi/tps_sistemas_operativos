a8ee8bbe93df481ed068965fe72d0ef560fd19e4
juampicarosi <juampicarosi@gmail.com>
2024-06-15 21:24:43 -0300

    fix test after merge

--- fisopfs/tests/escritura_archivos.sh
+++ fisopfs/tests/escritura_archivos.sh
@@ -2,11 +2,6 @@
 
 cd prueba
 
-echo "Nuevo contenido" > archivo2.txt
-echo "Contenido adicional" >> archivo2.txt
-
-cd prueba
-
 echo "Nuevo contenido" > archivo7.txt 2> /dev/null
 echo "Contenido adicional" >> archivo7.txt 2> /dev/null
 
